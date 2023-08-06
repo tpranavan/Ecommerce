385852fb502e7874f07cf75e0d3c6382adc7ea3d
it20066802 <it20066802@my.sliit.lk>
2023-08-07 00:13:35 +0530

    modifications in  Entitiy framwork iport

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -9,11 +9,15 @@ namespace API.Data
 {
     public class StoreContext : DbContext
     {
+        //parameterzed Constructor for StoreContext
         public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        //parameter in the constructor is used to configure the context.
         {
         }
 
+//DbSet is an Entity Framework Core feature that represents a table or a collection in the database.
         public DbSet<Product> Products { get; set; }
+        //Product is a entity class Product.cs
 
     }
 
